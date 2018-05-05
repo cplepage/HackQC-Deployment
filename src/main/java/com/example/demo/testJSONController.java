3c3ba862b183ddfd8b1d5f68d15bd3afac17677b
CP Lepage <lepage.charlesphilippe@gmail.com>
2018-05-05 12:56:36 -0400

    update CORS

--- src/main/java/com/example/demo/testJSONController.java
+++ src/main/java/com/example/demo/testJSONController.java
@@ -2,16 +2,16 @@ package com.example.demo;
 
 
 import org.json.JSONException;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 public class testJSONController {
-
-    @CrossOrigin("http://localhost:8080")
-    @RequestMapping(value = "/api/JSON", produces = "application/json")
+    @Autowired
+    @RequestMapping(value = "/JSON", produces = "application/json")
     public @ResponseBody testJSON testJSONController() throws JSONException {
         return new testJSON();
     }
