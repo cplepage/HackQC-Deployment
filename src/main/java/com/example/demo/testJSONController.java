56d836a5a4562c0ff35599151de6bec22a057e65
CP Lepage <lepage.charlesphilippe@gmail.com>
2018-05-05 10:38:11 -0400

    CORS enabling

--- src/main/java/com/example/demo/testJSONController.java
+++ src/main/java/com/example/demo/testJSONController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import org.json.JSONException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testJSONController {
 
-
+    @CrossOrigin("http://localhost:8080")
     @RequestMapping(value = "/api/JSON", produces = "application/json")
     public @ResponseBody testJSON testJSONController() throws JSONException {
         return new testJSON();
