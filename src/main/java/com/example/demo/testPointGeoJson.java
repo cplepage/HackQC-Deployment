55050e6aa35d8e657e4ae4840fbad12f302999f9
CP Lepage <lepage.charlesphilippe@gmail.com>
2018-05-04 22:06:15 -0400

    point test

--- src/main/java/com/example/demo/testPointGeoJson.java
+++ src/main/java/com/example/demo/testPointGeoJson.java
@@ -1,23 +1,51 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class testPointGeoJson {
 
-    private final String type;
-    private final float[] coordinates = new float[2];
+    private JSONObject point = new JSONObject();
 
-    public testPointGeoJson(){
-        this.type = "Point";
-        this.coordinates[0] = -71.49859980203153f;
-        this.coordinates[1] = 48.67906237862251f;
+    public testPointGeoJson() throws JSONException {
+
+        this.point.put("type", "Feature");
+
+        JSONObject properties = new JSONObject();
+        properties.put("date_observation", "2017/04/27 00:00:00");
+        properties.put("code_municipalite", "93055");
+        properties.put("nom", "Labrecque");
+        properties.put("urgence", "Inconnue");
+        properties.put("certitude", "Observé");
+        properties.put("type", "Inondation");
+        properties.put("type", "Mineure");
+        properties.put("type", "Actuel");
+
+        this.point.put("properties", properties);
+
+        JSONObject geometry = new JSONObject();
+        geometry.put("type", "Point");
+        JSONArray coordinates = new JSONArray();
+        coordinates.put(-71.49859980203153);
+        coordinates.put(48.67906237862251f);
+        geometry.put("coordinates", coordinates);
+
+        this.point.put("geometry", geometry);
+
+
 
     }
 
-    public String getType() {
-        return type;
-    }
 
-    public float[] getCoordinates() {
-        return coordinates;
+    @JsonRawValue
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty(value = "PointTest")
+    public String getPoint() {
+        return this.point.toString();
     }
 
 
