cd14e70b1fade4e48b004f92ecdd6657df6ae421
CP Lepage <lepage.charlesphilippe@gmail.com>
2018-05-05 18:05:45 -0400

    CORS update

--- src/main/java/hackqc18/Acclimate/AlertesController.java
+++ src/main/java/hackqc18/Acclimate/AlertesController.java
@@ -1,15 +1,18 @@
 package hackqc18.Acclimate;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 public class AlertesController {
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value="/alertes",produces="application/json")
+//    @Autowired
+    @RequestMapping(value="/alertes",produces="application/json;charset=UTF-8")
     public String alertes(
             @RequestParam(value="nord", defaultValue="63.") double nord,
             @RequestParam(value="sud", defaultValue="40.") double sud,
